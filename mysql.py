a6ceaabef84ec54f0eae034541f8dd844fba2de1
Gabriel Oliveira <117647436+gbyuri@users.noreply.github.com>
2023-02-10 00:29:26 -0300

    Add files via upload

---
+++ mysql.py
@@ -0,0 +1,15 @@
+import json 
+with open('data.json') as data_json:
+    data=json.load(data_json)
+    
+    
+
+import pymysql.cursors
+connection = pymysql.connect(host='127.0.0.1',user='root',password='963258854',database='teste')
+with connection:
+    with connection.cursor() as cursor:
+        print("Connected!")
+        print(data['-NNDEL_L4QfRInEn5Im6']['datetime'])
+        sql = 'INSERT INTO tableteste (dataid) VALUE (%s)'
+        cursor.execute(sql,(1))
+        
